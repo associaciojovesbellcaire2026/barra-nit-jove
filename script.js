1694822b4411ebbf458d8f98cc4b57f3fa294dec
associaciojovesbellcaire2026 <associaciojovesbellcaire@gmail.com>
2026-06-30 02:55:10 +0200

    versió 8 crec

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let total = 0;
+let pagat = 0;
 let productes = {
     'Cervesa 🍺': { preu: 2.5, count: 0, class: 'groc' },
     'Combinat 🍹': { preu: 6.0, count: 0, class: 'vermell' },
@@ -14,25 +15,28 @@ function render() {
     for (let nom in productes) {
         let p = productes[nom];
         grid.innerHTML += `<button class="producte ${p.class}" onclick="afegir('${nom}')">
-            ${nom} <span class="comptador">${p.count}</span>
-            <button class="btn-minus" onclick="event.stopPropagation(); restar('${nom}')">-</button>
+            ${nom}<span class="preu-petit">${p.preu.toFixed(2)}€</span>
+            <span class="comptador">${p.count}</span>
         </button>`;
     }
     document.getElementById("total").innerHTML = total.toFixed(2) + " €";
 }
 
 function afegir(nom) { total += productes[nom].preu; productes[nom].count++; render(); }
-function restar(nom) { if (productes[nom].count > 0) { total -= productes[nom].preu; productes[nom].count--; render(); } }
-function reiniciar() { total = 0; for(let n in productes) productes[n].count = 0; render(); }
+function reiniciar() { total = 0; pagat = 0; for(let n in productes) productes[n].count = 0; render(); }
 function pagar(tipus) {
-    if (tipus === 'Targeta') { alert("Venda amb targeta registrada!"); reiniciar(); }
-    else { document.getElementById('pantallaPrincipal').style.display = 'none'; document.getElementById('pantallaEfectiu').style.display = 'block'; }
+    if (tipus === 'Targeta') { alert("Venda amb targeta!"); reiniciar(); }
+    else { 
+        document.getElementById('pantallaPrincipal').style.display = 'none'; 
+        document.getElementById('pantallaEfectiu').style.display = 'block';
+        actualitzarPantallaEfectiu();
+    }
 }
-function calcularCanvi() {
-    let rebut = parseFloat(document.getElementById('input-manual').value) || 0;
-    document.getElementById('display-canvi').innerHTML = `Total: ${total.toFixed(2)}€ | Torna: ${Math.max(0, rebut - total).toFixed(2)}€`;
+function sumarEfectiu(val) { pagat += val; actualitzarPantallaEfectiu(); }
+function actualitzarPantallaEfectiu() {
+    document.getElementById('display-canvi').innerHTML = 
+    `Total: ${total.toFixed(2)}€ | Rebut: ${pagat.toFixed(2)}€ <br> <b>Torna: ${Math.max(0, pagat - total).toFixed(2)}€</b>`;
 }
 function tornar() { document.getElementById('pantallaPrincipal').style.display = 'flex'; document.getElementById('pantallaEfectiu').style.display = 'none'; }
-function finalitzar() { alert('Venda registrada'); reiniciar(); tornar(); }
-render();
+function finalitzar() { alert('Venda finalitzada'); reiniciar(); tornar(); }
 render();
